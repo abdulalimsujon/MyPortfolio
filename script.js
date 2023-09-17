cc0ffba6e3ed98af2ac196ca72207ca44ddffd07
unknown <alimsujon12@gmail.com>
2023-09-17 12:28:53 -0700

    jdg

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var typed = new typed(".text",{
-    strings:["Fonted-developer",'Backend-developer'],
+var typed = new Typed('#text', {
+    strings:["Fronted-developer",'Backend-developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
-})
+  });
